fcf25cfb1d02bc44d600d5958d9e81a059e2b201
SST-GH <97111715+SST-GH@users.noreply.github.com>
2022-05-03 17:32:50 +0100

    s

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
 const pokemonListUrl = "https://pokeapi.co/api/v2/pokemon?limit=150";
+
+const image = document.querySelector(".poki");
+const container = document.querySelector(".container");
+const showAll = document.querySelector(".btn1");
+
+const renderPokemon = function (data, className = "") {
+  const html = `<article class="name ${className}">
+      <img class="pokemon__img" src="${data.results}" />
+      <div class="pokemon__data">
+        <h3 class="pokemon__name">${data.results}</h3>
+      </div>
+    </article>`;
+  container.insertAdjacentHTML("beforeend", html);
+};
+
+const getJSON = function (url, errorMessage = "Something went wrong") {
+  return fetch(url).then(function (response) {
+    if (!response.ok) {
+      throw new Error(`${errorMessage} ${response.status}`);
+    }
+
+    return response.json();
+  });
+};
+
+const renderError = function (message) {
+  container.insertAdjacentText("beforeend", message);
+};
+
+const getPokemon = function () {
+  getJSON(`https://pokeapi.co/api/v2/pokemon?limit=150`, `Pokemon not found`)
+    .then(function (data) {
+      console.log(data.results);
+
+      const name = data.results.map(function (name) {
+        return name.name;
+      });
+      console.log(name);
+      renderPokemon(name);
+
+      if (!name) {
+        throw new Error("No name found!");
+      }
+
+      const image = data.results.map(function (url) {
+        return url.url;
+      });
+      console.log(image);
+      renderPokemon(image);
+
+      if (!image) {
+        throw new Error("No image found!");
+      }
+
+      return getJSON(
+        `https://pokeapi.co/api/v2/pokemon?offset=150`,
+        `pokemon not found`
+      );
+    })
+    .then(function (data) {
+      return renderPokemon(data, "name");
+    })
+    .then(function (data) {
+      return renderPokemon(data, "image");
+    })
+    .catch(function (error) {
+      return renderError(`Something went wrong ${error.message}!`);
+    });
+};
+
+getPokemon();
+
+// showAll.addEventListener("click", function () {
+//   getPokemon("");
+// });
